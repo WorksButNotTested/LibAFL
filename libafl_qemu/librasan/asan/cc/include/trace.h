d530b86e1ff0703477f9fd002d5f838990d9aa11
Your Name <you@example.com>
2025-02-25 13:59:57 +0000

    Fix C formatting

--- libafl_qemu/librasan/asan/cc/include/trace.h
+++ libafl_qemu/librasan/asan/cc/include/trace.h
@@ -1,6 +1,6 @@
 #ifndef _LOG_H_
 #define _LOG_H_
 
-void trace(const char* fmt, ...);
+void trace(const char *fmt, ...);
 
 #endif
--- libafl_qemu/librasan/asan/cc/src/log.c
+++ libafl_qemu/librasan/asan/cc/src/log.c
@@ -4,15 +4,13 @@
 
 static char log_buffer[PATH_MAX] = {0};
 
-extern void log_trace(char * msg);
+extern void log_trace(char *msg);
 
-void trace(const char* fmt, ...) {
+void trace(const char *fmt, ...) {
   va_list va;
   va_start(va, fmt);
   int len = vsnprintf_(log_buffer, sizeof(log_buffer), fmt, va);
-  if (len > 0) {
-    log_trace(log_buffer);
-  }
+  if (len > 0) { log_trace(log_buffer); }
   va_end(va);
 }
 
